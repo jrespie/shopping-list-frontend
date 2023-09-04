8dbed37c7283bdedd4411f9873efb932d1efc448
James Espie <jamesespie@gmail.com>
2023-09-04 21:32:31 +1200

    feat: turn list items into rounded buttons

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 import './index.css';
+import './styles/styles.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
